a4e031a2cd3eb81a9baebc4096ec54175db91908
Awilum <awilum@msn.com>
2018-06-13 01:10:34 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# page-var
+# Page Variables Plugin for [Flextype](http://flextype.org/)
+![version](https://img.shields.io/badge/version-1.0.0-brightgreen.svg?style=flat-square)
+![Flextype](https://img.shields.io/badge/Flextype-0.x-green.svg?style=flat-square)
+![MIT License](https://img.shields.io/badge/license-MIT-blue.svg?style=flat-square)
+
 Page Variables plugin provides a basic way to craete and access page variables.
+
+## Installation
+1. Unzip plugin to the folder `/site/plugins/`
+2. Go to `/site/config/site.yaml` and add plugin name to plugins section.
+3. Save your changes.
+
+Example:
+```
+plugins:
+  - page-var
+```
+
+## Usage in page content
+
+Set page variable
+```
+[var_set name="variable-name" value="variable value"]
+```
+
+Get page variable
+```
+[var_get name="variable-name"]
+```
+
+## Usage in the template
+
+Define Flextype namespace in the template if it is not defined yet.
+```
+<?php namespace Flextype; ?>
+```
+
+Display page variable
+```
+<?php echo Variables::get('variable-name'); ?>
+```
+
+## Settings
+
+```yaml
+enabled: true # or `false` to disable the plugin
+```
+
+## License
+See [LICENSE](https://github.com/flextype-plugins/page-var/blob/master/LICENSE)
---
+++ page-var.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ *
+ * Flextype Page Variables Plugin
+ *
+ * @author Romanenko Sergey / Awilum <awilum@yandex.ru>
+ * @link http://flextype.org
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Flextype;
+
+use Thunder\Shortcode\Shortcode\ShortcodeInterface;
+use Flextype\Component\{Event\Event, Registry\Registry};
+
+// Event: onShortcodesInitialized
+Event::addListener('onShortcodesInitialized', function () {
+
+    // Shortcode: [var_set name="variable-name" value="variable value"]
+    Content::shortcode()->addHandler('var_set', function(ShortcodeInterface $s) {
+        Registry::set('variables.page_var_'.$s->getParameter('name'), $s->getParameter('value'));
+    });
+
+    // Shortcode: [var_get name="variable-name"]
+    Content::shortcode()->addHandler('var_get', function(ShortcodeInterface $s) {
+        return Registry::get('variables.page_var_'.$s->getParameter('name'));
+    });
+});
+
+class Variables
+{
+    /**
+     * Get page variable value
+     *
+     * echo Variables::get('variable-name');
+     *
+     * @access public
+     * @param  string  $variable_name  Variable name
+     * @return mixed
+     */
+    public static function get(string $variable_name)
+    {
+        return Registry::get('variables.page_var_'.$variable_name);
+    }
+}
